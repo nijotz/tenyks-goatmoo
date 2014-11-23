cb9b7a711dbeefc8ff2ff441bbab9e634ab193c1
Nick Tzaperas <nick@nijotz.com>
2014-11-23 00:21:08 -0800

    Initial commit

---
+++ goatmoo.py
@@ -0,0 +1,41 @@
+from tenyksservice import TenyksService, run_service, FilterChain
+from goattower.engine import handle_text, get_text, session
+from goattower.models import Actor, User
+
+
+class GoatMOO(TenyksService):
+    irc_message_filters = {
+        'goat': FilterChain([r"^(?i)goat ?(?P<cmd>(.*))?$"], private_only=True)
+    }
+
+    help_text = 'help'
+
+    def handle_goat(self, data, match):
+        nick = data['nick']
+
+        user = session.query(User).filter(User.name == nick).first()
+        if not user:
+            actor = Actor(name=nick)
+            actor.parent_id = 1
+            user = User(name=nick, actor=actor)
+            session.add(actor)
+            session.add(user)
+            session.commit()
+
+        cmd = match.groupdict()['cmd']
+        if not cmd:
+            cmd = 'look'
+
+        self.logger.debug('Goat command: {cmd}'.format(cmd=cmd))
+        handle_text(user.actor.id, cmd)
+
+        for line in get_text(user.actor.id):
+            self.send(line.replace('\n', ''), data)
+
+
+def main():
+    run_service(GoatMOO)
+
+
+if __name__ == '__main__':
+    main()
